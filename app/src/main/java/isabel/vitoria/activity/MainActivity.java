7716ac1098624a88f981b4100238fa3d8045baa0
20212TIOT0111 <vitoria.l.s.s2018@gmail.com>
2024-05-16 18:32:37 -0300

    aula2

--- app/src/main/java/isabel/vitoria/activity/MainActivity.java
+++ app/src/main/java/isabel/vitoria/activity/MainActivity.java
@@ -5,21 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.view.View;
+
 
 import isabel.vitoria.galeria.R;
 
-public class PhotoActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo);
+        setContentView(R.layout.activity_main);
 
-        Toolbar toolbar= findViewById(R.id.tbPhoto); //obtem o elemento tbPhoto
+        Toolbar toolbar = findViewById(R.id.tbMain); //obtem o elemento tbPhoto
         setSupportActionBar(toolbar); //indica que deve ser considerado como a ActionBar padrão da tela
-
-        ActionBar actionBar = getSupportActionBar(); //obtém a ActionBar padrã0
-        actionBar.setDisplayHomeAsUpEnabled(true); //habilita o botão de voltar na ActionBar
     }
 }
--- app/src/main/res/values/galeria/MainActivity.java
+++ app/src/main/res/values/galeria/MainActivity.java
@@ -3,6 +3,7 @@ package values.galeria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import isabel.vitoria.galeria.R;
 
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tbMain); //obtem o elemento tbMain
+        setSupportActionBar(toolbar); //indica que deve ser considerado como a ActionBar padrão da tela
     }
 }
